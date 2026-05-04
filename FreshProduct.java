11da0520de190221f45579ad82981342b55b7480
bassamalharthy7 <bassamalharthy7@gmail.com>
2026-05-04 19:33:15 +0300

    Update FreshProduct.java

--- FreshProduct.java
+++ FreshProduct.java
@@ -1,5 +1,24 @@
 package OOP;
 
-public class FreshProduct {
 
+
+//للوراثة extends
+public class FreshProduct extends Product {
+    
+    // تاريخ الانتهاء 
+    private String expiryDate;
+
+    //  (Constructor)
+    public FreshProduct(String code, String name, double price, Category category, String expiryDate) {
+        // استدعاء (constructor) الكلاس الأساسي (Product) باستخدام super
+        super(code, name, price, category);
+        this.expiryDate = expiryDate;
+    }
+
+    // دالة toString لاظهار تاريخ الانتهاء 
+    @Override
+    public String toString() {
+        //  super.toString() للحصول على بيانات المنتج الأساسية بعدها نضيف التاريخ
+        return super.toString() + ", Expiry Date: " + expiryDate;
+    }
 }
